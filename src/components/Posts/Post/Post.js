2790d6ae2d1246c21585dad35c4f97f8c48431ce
MuGen9 <maksparadowski@gmail.com>
2022-02-24 19:40:45 +0100

    posts displaying

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,10 +1,26 @@
+import moment from 'moment';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+
 import useStyles from './styles';
 
-const Post = () => {
+const Post = ({ post }) => {
   const classes = useStyles();
-  
   return (
-    <div>Post</div>
+    <Card className={classes.card}>
+      <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+      <div className={classes.overlay}>
+        <Typography variant='h6'>{post.creator}</Typography>
+        <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{color: 'white'}} size='small' onClick={() => {}}>
+          <MoreHorizIcon fontSize='medium' />
+        </Button>
+      </div>
+    </Card>
   )
 }
 
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,3 +1,4 @@
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
@@ -10,12 +11,16 @@ const Posts = () => {
   console.log(posts);
 
   return (
-    <>
-        <h1>POSTS</h1>
-        <Post />
-        <Post />
-    </>
-  )
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.mainContainer} container alignItems='stretch' spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
+  );
 }
 
 export default Posts
