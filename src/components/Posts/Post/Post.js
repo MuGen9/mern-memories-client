9318f10c72263a9d7365d85f76297f2e253aacb5
MuGen9 <maksparadowski@gmail.com>
2022-02-25 14:52:10 +0100

    finished post component

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -8,6 +8,7 @@ import useStyles from './styles';
 
 const Post = ({ post }) => {
   const classes = useStyles();
+  
   return (
     <Card className={classes.card}>
       <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -20,6 +21,23 @@ const Post = ({ post }) => {
           <MoreHorizIcon fontSize='medium' />
         </Button>
       </div>
+      <div className={classes.details}>
+        <Typography variant='body2' color='textSecondary'>{post.tags.map((tag) => `#${tag} `)}</Typography>
+      </div>
+      <CardContent>
+        <Typography className={classes.title} variant='h5' gutterBottom>{post.message}</Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size='small' color='primary' onClick={() => {}}>
+          <ThumbUpAltIcon fontSize='small' />
+          Like
+          {post.likeCount}
+        </Button>
+        <Button size='small' color='secondary' onClick={() => {}}>
+          <DeleteIcon fontSize='small' />
+          Delete
+        </Button>
+      </CardActions>
     </Card>
   )
 }
