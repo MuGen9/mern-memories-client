d5a4fbc39dec72c0724fefe44586c02398f2f76b
MuGen9 <maksparadowski@gmail.com>
2022-03-01 20:56:26 +0100

    delete post

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -3,10 +3,14 @@ import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import { useDispatch } from 'react-redux';
+
+import { deletePost } from '../../../actions/posts';
 
 import useStyles from './styles';
 
 const Post = ({ post, setCurrentId }) => {
+  const dispatch = useDispatch();
   const classes = useStyles();
   
   return (
@@ -34,7 +38,7 @@ const Post = ({ post, setCurrentId }) => {
           Like
           {post.likeCount}
         </Button>
-        <Button size='small' color='secondary' onClick={() => {}}>
+        <Button size='small' color='secondary' onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize='small' />
           Delete
         </Button>
