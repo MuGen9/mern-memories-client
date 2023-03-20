775a08d9c89bfa6e7bdee10834d069e187b3b743
Maks Paradowski <50915494+MuGen9@users.noreply.github.com>
2023-03-20 15:09:44 +0100

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://mern-memories-serv.herokuapp.com/posts';
+const url = 'https://ill-plum-turkey-tux.cyclic.app/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
